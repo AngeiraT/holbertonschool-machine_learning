00baef6834bc3af0ae60e357e3f592c4bb8c7119
AngeiraT <thaispur04@icloud.com>
2023-03-22 11:12:36 -0400

    claculates the shape of numpy

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module shape of numpy.ndarray
+"""
+
+
+import numpy as np
+
+
+def np_shape(matrix):
+    """
+    Calculate Shape of numpy.ndarray
+    Parameters:
+        matrix (numpy.ndarray): The numpy ndarray for which to calculate the shape.
+        
+    Returns:
+        tuple: The shape of the numpy ndarray as a tuple of integers.
+    """
+    return tuple(matrix.shape)
