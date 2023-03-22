8f8752db333b2d7ca1d8567fa63104a4df11836d
AngeiraT <thaispur04@icloud.com>
2023-03-22 08:43:56 -0400

    whitespace

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -12,10 +12,10 @@ def mat_mul(mat1, mat2):
         return None
 
     newMatrix = [[0 for j in range(len(mat2[0]))] for i in range(len(mat1))]
-    
+
     for i in range(len(mat1)):
         for j in range(len(mat2[0])):
             for k in range(len(mat2)):
                 newMatrix[i][j] += mat1[i][k] * mat2[k][j]
-    
+ 
     return newMatrix
