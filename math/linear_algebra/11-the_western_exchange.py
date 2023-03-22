72f0057dd879ba05be2053d590a9b695550dcff8
AngeiraT <thaispur04@icloud.com>
2023-03-22 11:31:32 -0400

    transpose of numpy

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module transpose of numpy.ndarray
+"""
+
+
+def np_transpose(matrix):
+    """
+    Transpose of numpy.ndarray
+    Parameters:
+        matrix (numpy.ndarray): The numpy ndarray
+
+    Returns:
+        a new numpy.nd.array
+    """
+    return matrix.transpose()
