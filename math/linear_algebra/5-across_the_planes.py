93dc69518b86c93e4d1f8609709f5fd5664c5de9
AngeiraT <thaispur04@icloud.com>
2023-03-21 23:27:51 -0400

    under indented

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -13,4 +13,4 @@ def add_matrices2D(mat1, mat2):
         return None
     else:
         return [[mat1[i][j] + mat2[i][j]
-for j in range(len(mat1[0]))] for i in range(len(mat1))]
+    for j in range(len(mat1[0]))] for i in range(len(mat1))]
