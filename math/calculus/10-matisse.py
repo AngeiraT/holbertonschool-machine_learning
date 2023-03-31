fe539ed533743833e704aaea44b2813fcaf83d6c
AngeiraT <4985@holbertonstudents.com>
2023-03-31 13:29:06 -0400

    derivative of a poly

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+module poly_derivative
+"""
+
+
+def poly_derivative(poly):
+    """
+    calculates the derivative of a polynomial
+    Args:
+        poly (list): list of coefficients representing a polynomial
+    Returns:
+        list: new list of coefficients representing the derivative of the
+        polynomial
+    """
+    if type(poly) is not list:
+        return None
+    if len(poly) == 1:
+        return [0]
+    if len(poly) == 0:
+        return None
+    derivative = []
+    for power in range(1, len(poly)):
+        if type(poly[power]) is not int:
+            return None
+        derivative.append(poly[power]*power)
+    return derivative
