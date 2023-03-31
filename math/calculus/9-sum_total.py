1569b41e86327a87cc95e944a4aea0022a50964b
AngeiraT <4985@holbertonstudents.com>
2023-03-31 13:25:45 -0400

    sumation i squared

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+module summation_i_squared
+"""
+
+
+def summation_i_squared(n):
+    """
+    calculates summation
+    Args:
+        n (int): stopping condition
+    Returns:
+        int: sum
+    """
+    if n > 0:
+        return int(n*(n+1)*((2*n)+1)/6)
+    return None
