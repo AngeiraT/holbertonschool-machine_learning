8bb9ed5612ffc63b363900b6acc13672008f225f
AngeiraT <4985@holbertonstudents.com>
2023-04-21 12:09:58 -0400

    forward prop

---
+++ supervised_learning/classification/10-neural_network.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+"""Script to create an FNN with private attributes """
+
+import numpy as np
+
+
+class NeuralNetwork():
+    """ Class neural network"""
+
+    def __init__(self, nx, nodes):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        # Initialize weights and biases
+        # Hidden layer
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        # Output Neuron
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        """
+        Getter attr
+        Args:
+            self: Private attribute
+            Returns: Weight vector 1 hidden layer
+        """
+        return self.__W1
+
+    @property
+    def b1(self):
+        """
+        Getter attr
+        Args:
+            self: Private attribute
+            Returns: Bias1
+        """
+        return self.__b1
+
+    @property
+    def A1(self):
+        """
+        Getter attr
+        Args:
+            self: Private attribute
+            Returns: Activated1
+        """
+        return self.__A1
+
+    @property
+    def W2(self):
+        """
+        Getter attr
+        Args:
+            self: Private attribute
+            Returns: Weight vector 2
+        """
+        return self.__W2
+
+    @property
+    def b2(self):
+        """
+        Getter attr
+        Args:
+            self: Private attribute
+            Returns: Bias2
+        """
+        return self.__b2
+
+    @property
+    def A2(self):
+        """
+        Getter attr
+        Args:
+            self: Private attribute
+            Returns: Activated output 2 prediction
+        """
+        return self.__A2
+
+    def forward_prop(self, X):
+        """
+        Method to calculate a forward propagation in a FNN
+        Args:
+            X: input data vector
+        Returns: A1 and A2 the activation nodes using sigmoid
+        """
+        Z1 = np.matmul(self.__W1, X) + self.__b1
+        sigmoid_1 = 1 / (1 + np.exp(-Z1))
+        self.__A1 = sigmoid_1
+        Z2 = np.matmul(self.__W2, self.__A1) + self.__b2
+        sigmoid_2 = 1 / (1 + np.exp(-Z2))
+        self.__A2 = sigmoid_2
+
+        return self.__A1, self.__A2
