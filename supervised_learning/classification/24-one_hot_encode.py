7a86338382ebbb66e2f8a44bfd48092a93ca77ca
AngeiraT <4985@holbertonstudents.com>
2023-04-24 23:47:44 -0400

    one hot matrix

---
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Script one hot encode model
+"""
+
+import numpy as np
+
+
+def one_hot_encode(Y, classes):
+    """
+    Converts a numeric label vector into a one-hot matrix
+    Args:
+        Y (ndarray): contains numeric class labels
+        classes (int): maximum number of classes found in Y
+    Returns:
+        ndarray: one-hot encoding of Y or None on failure
+    """
+    if not isinstance(Y, np.ndarray) or len(Y.shape) != 1:
+        return None
+    if not isinstance(classes, int) or classes <= 0:
+        return None
+    m = Y.shape[0]
+    Y_one_hot = np.zeros((classes, m))
+    Y_one_hot[Y, np.arange(m)] = 1
+    return Y_one_hot
