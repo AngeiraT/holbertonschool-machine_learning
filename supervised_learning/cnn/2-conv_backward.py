fe1027b4fed96a45bb1c1b53ef1d3efe13982d26
AngeiraT <4985@holbertonstudents.com>
2023-05-21 20:43:30 -0400

    dimensions of the arrays are not compatible

--- supervised_learning/cnn/2-conv_backward.py
+++ supervised_learning/cnn/2-conv_backward.py
@@ -23,7 +23,7 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     # Retrieve shapes
     m, h_new, w_new, c_new = dZ.shape
     m, h_prev, w_prev, c_prev = A_prev.shape
-    kh, kw, _, _ = W.shape
+    kh, kw, c_prev, c_new = W.shape
     sh, sw = stride
 
     # Initialize output gradients
@@ -35,7 +35,9 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     if padding == "same":
         pad_h = int(np.ceil(((h_prev - 1) * sh + kh - h_prev) / 2))
         pad_w = int(np.ceil(((w_prev - 1) * sw + kw - w_prev) / 2))
-        A_prev_pad = np.pad(A_prev, ((0, 0), (pad_h, pad_h), (pad_w, pad_w), (0, 0)), mode='constant')
+        A_prev_pad = np.pad(A_prev, ((0, 0),
+                                     (pad_h, pad_h), (pad_w, pad_w), (0, 0)),
+                            mode='constant')
     else:
         pad_h = pad_w = 0
         A_prev_pad = A_prev
